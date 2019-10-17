539115ceca47e6483aa5c7c3c1239171d655a53b
cassie-cassie <2896165117@qq.com>
2019-10-17 19:18:34 +0800

    修改路由

--- third-stage/routes/index.js
+++ third-stage/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Router = express.Router();
 
-首页路由
+// // 首页路由
 Router.get('/',require('../controller/nav.js'))
 
 // // 登录路由
@@ -22,8 +22,8 @@ Router.get('/',require('../controller/nav.js'))
 // // 国际
 // Router.get('/international',require('../controller/international'))
 
-// // // 家电
-// Router.get('/homeapp',require('../controller/homeapp'))
+// // 家电
+Router.get('/homeapp',require('../controller/homeapp'))
 
 // // 家居
 // Router.get('/furniture',require('../controller/furniture'))
