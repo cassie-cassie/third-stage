553936d11bb37fb2a186c67c24d383dfa6d5ff11
cassie-cassie <2896165117@qq.com>
2019-10-20 16:33:57 +0800

    首页详情

---
+++ third-stage/controller/index/womenclothes.js
@@ -0,0 +1,36 @@
+function showWomenClothes(req,res){
+    res.render('womenclothes',{
+        data:[{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/21/32/ias_2664a882-0973-42ce-ad6b-8235d2ce1acd_604x290_100.jpg!75.webp",
+            sell:'MAPLE AROMA女装特卖专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/06/11/116/ias_b264e46a-f01e-49e3-afa8-bc060e97228f_604x290_100.jpg!75.webp",
+            sell:'BUOUBUOU女装品质专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/10/19/146/ias_234814b2-ea92-4e8d-8689-2fda47075ae1_604x290_100.jpg!75.webp",
+            sell:'TITI女装品质专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/06/194/ias_196f41ce-2e9c-4f05-9afa-b0417c8e20b4_604x290_100.jpg!75.webp",
+            sell:'蓝狄斯丹女装专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/10/15/20/ias_e4d87a9a-0d6c-4e80-8f09-142a27da9299_604x290_100.jpg!75.webp",
+            sell:'多品牌女装联促聚献专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/23/53/ias_4b8c119c-3bd1-4785-a6ab-4b4f0fa1ff6d_604x290_100.jpg!75.webp",
+            sell:'歌力思裙装品质专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/01/61/ias_d6e57aa8-c663-4fb5-89cc-3f97d3e4feb3_604x290_100.jpg!75.webp",
+            sell:'卡洛琳女装专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/03/178/ias_156750257090726_604x290_100.jpg!75.webp",
+            sell:'WANCAOYI女装好货超值购'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/05/59/ias_5a4df695-3753-4b7b-8720-02f23b47e060_604x290_100.jpg!75.webp",
+            sell:'精选多品牌女装品质专场'
+        },{
+            img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/06/03/84/ias_76250f57-8ec1-466e-abff-1ebcf00d5f8c_604x290_100.jpg!75.webp",
+            sell:'OBBLIGATO女装特卖专场'
+        }]
+    })
+}
+module.exports = showWomenClothes
