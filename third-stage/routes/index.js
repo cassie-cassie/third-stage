26b26b7cfc35168099b641857ee5c268d7b237a8
cassie-cassie <2896165117@qq.com>
2019-10-21 17:36:33 +0800

    新增分类路由

--- third-stage/routes/index.js
+++ third-stage/routes/index.js
@@ -6,6 +6,9 @@ Router.get('/index',require('../controller/nav.js'))
 // 首页女装详情
 Router.get('/index/womenclothes',require('../controller/index/womenclothes'))
 
+// 购物车
+Router.get('/shoppingcar',require('../controller/shoppingcar'))
+
 // 登录路由
 Router.get('/login',require('../controller/login.js'))
 Router.get('/login_user',require('../controller/login_user'))
@@ -36,5 +39,8 @@ Router.get('/life',require('../controller/life'))
 
 // // 唯品奢
 Router.get('/vipcostly',require('../controller/vipcostly'))
+
+// 分类
+Router.get('/index/classify',require('../controller/classify'))
 
 module.exports = Router
