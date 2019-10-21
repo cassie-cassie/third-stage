9645b8a3b9bbbe622ff9746548eb80fde1bdb31e
cassie-cassie <2896165117@qq.com>
2019-10-21 15:02:07 +0800

    取消注释

--- third-stage/routes/index.js
+++ third-stage/routes/index.js
@@ -16,20 +16,20 @@ Router.get('/user',require('../controller/user.js'))
 // // 最后疯抢
 Router.get('/lastcrazy',require('../controller/lastcrazy'))
 
-// // 美妆
-// Router.get('/newbeauty',require('../controller/newbeauty'))
+// 美妆
+Router.get('/newbeauty',require('../controller/newbeauty'))
 
-// // 母婴
-// Router.get('/baby',require('../controller/baby'))
+// 母婴
+Router.get('/baby',require('../controller/baby'))
 
-// // 国际
-// Router.get('/international',require('../controller/international'))
+// 国际
+Router.get('/international',require('../controller/international'))
 
 // // 家电
 Router.get('/homeapp',require('../controller/homeapp'))
 
-// // 家居
-// Router.get('/furniture',require('../controller/furniture'))
+// 家居
+Router.get('/furniture',require('../controller/furniture'))
 
 // 生活
 Router.get('/life',require('../controller/life'))
