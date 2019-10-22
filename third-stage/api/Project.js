5bd5786940603ceb7be306035c8bc747efdd40af
cassie-cassie <2896165117@qq.com>
2019-10-22 15:46:28 +0800

    修改api的project.js

--- third-stage/api/Project.js
+++ third-stage/api/Project.js
@@ -1,21 +1,148 @@
 const Project = require('../model/Project')
 
 function showProject(req,res){
-    // Project.create({type:'123'},(err)=>{
-    //     if(!err){
-    //         console.log('888')
-    //     }else{
-    //         console.log(err)
-    //     }
-    // })
     Project.create({
         type:'女装',
-        img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/21/32/ias_2664a882-0973-42ce-ad6b-8235d2ce1acd_604x290_100.jpg!75.webp",
-        sell:'MAPLE AROMA女装特卖专场'
+        imgdesc:'/images/index/indexList1.png',
+        list:[{
+            smallimage:'/images/index/womenclothes(1).webp',
+            listdesc:'连衣裙'
+        },{
+            smallimage:'/images/index/womenclothes(2).webp',
+            listdesc:'百搭外套'
+        },{
+            smallimage:'/images/index/womenclothes(3).webp',
+            listdesc:'套装'
+        },{
+            smallimage:'/images/index/womenclothes(4).webp',
+            listdesc:'针织毛衣'
+        },{
+            smallimage:'/images/index/womenclothes(5).webp',
+            listdesc:'卫衣'
+        },{
+            smallimage:'/images/index/womenclothes(6).webp',
+            listdesc:'衬衫'
+        },{
+            smallimage:'/images/index/womenclothes(7).webp',
+            listdesc:'大衣'
+        },{
+            smallimage:'/images/index/womenclothes(8).webp',
+            listdesc:'气质风衣'
+        },{
+            smallimage:'/images/index/womenclothes(9).webp',
+            listdesc:'高腰裤'
+        },{
+            smallimage:'/images/index/womenclothes(10).webp',
+            listdesc:'T恤'
+        },{
+            smallimage:'/images/index/womenclothes(11).webp',
+            listdesc:'半身裙'
+        },{
+            smallimage:'/images/index/womenclothes(12).webp',
+            listdesc:'羽绒服'
+        }],
+        largelist:[{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/06/194/ias_196f41ce-2e9c-4f05-9afa-b0417c8e20b4_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/06/11/116/ias_b264e46a-f01e-49e3-afa8-bc060e97228f_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/10/19/146/ias_234814b2-ea92-4e8d-8689-2fda47075ae1_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/06/194/ias_196f41ce-2e9c-4f05-9afa-b0417c8e20b4_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/10/15/20/ias_e4d87a9a-0d6c-4e80-8f09-142a27da9299_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/23/53/ias_4b8c119c-3bd1-4785-a6ab-4b4f0fa1ff6d_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/01/61/ias_d6e57aa8-c663-4fb5-89cc-3f97d3e4feb3_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/03/178/ias_156750257090726_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/05/59/ias_5a4df695-3753-4b7b-8720-02f23b47e060_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/06/03/84/ias_76250f57-8ec1-466e-abff-1ebcf00d5f8c_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        }]
     },{
-        type:'女装',
-        img:"https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/06/11/116/ias_b264e46a-f01e-49e3-afa8-bc060e97228f_604x290_100.jpg!75.webp",
-        sell:'BUOUBUOU女装品质专场'
+        type:'男装',
+        imgdesc:'/images/index/indexList1.png',
+        list:[{
+            smallimage:'/images/index/womenclothes(1).webp',
+            listdesc:'连衣裙'
+        },{
+            smallimage:'/images/index/womenclothes(2).webp',
+            listdesc:'百搭外套'
+        },{
+            smallimage:'/images/index/womenclothes(3).webp',
+            listdesc:'套装'
+        },{
+            smallimage:'/images/index/womenclothes(4).webp',
+            listdesc:'针织毛衣'
+        },{
+            smallimage:'/images/index/womenclothes(5).webp',
+            listdesc:'卫衣'
+        },{
+            smallimage:'/images/index/womenclothes(6).webp',
+            listdesc:'衬衫'
+        },{
+            smallimage:'/images/index/womenclothes(7).webp',
+            listdesc:'大衣'
+        },{
+            smallimage:'/images/index/womenclothes(8).webp',
+            listdesc:'气质风衣'
+        },{
+            smallimage:'/images/index/womenclothes(9).webp',
+            listdesc:'高腰裤'
+        },{
+            smallimage:'/images/index/womenclothes(10).webp',
+            listdesc:'T恤'
+        },{
+            smallimage:'/images/index/womenclothes(11).webp',
+            listdesc:'半身裙'
+        },{
+            smallimage:'/images/index/womenclothes(12).webp',
+            listdesc:'羽绒服'
+        }],
+        largelist:[{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/06/194/ias_196f41ce-2e9c-4f05-9afa-b0417c8e20b4_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/06/11/116/ias_b264e46a-f01e-49e3-afa8-bc060e97228f_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/10/19/146/ias_234814b2-ea92-4e8d-8689-2fda47075ae1_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/06/194/ias_196f41ce-2e9c-4f05-9afa-b0417c8e20b4_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/10/15/20/ias_e4d87a9a-0d6c-4e80-8f09-142a27da9299_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/23/53/ias_4b8c119c-3bd1-4785-a6ab-4b4f0fa1ff6d_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/08/01/61/ias_d6e57aa8-c663-4fb5-89cc-3f97d3e4feb3_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/03/178/ias_156750257090726_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/09/05/59/ias_5a4df695-3753-4b7b-8720-02f23b47e060_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        },{
+            largeimage:'https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2019/06/03/84/ias_76250f57-8ec1-466e-abff-1ebcf00d5f8c_604x290_100.jpg!75.webp',
+            sell:'蓝狄斯丹女装专场'
+        }]
     })
 }
 
