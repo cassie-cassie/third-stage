2f95a18e0d233abb36904aa0cda2623c3b12c15f
cassie-cassie <2896165117@qq.com>
2019-10-22 18:35:01 +0800

    修改女装js

--- third-stage/controller/index/womenclothes.js
+++ third-stage/controller/index/womenclothes.js
@@ -1,7 +1,7 @@
 
 const project = require('../../model/Project')
 function showWomenClothes(req,res){
-    project.find({"type":"男装"}).then((results)=>{
+    project.find({"type":"运动户外"}).then((results)=>{
         if(results){
             console.log(results)
             res.render('womenclothes',{
