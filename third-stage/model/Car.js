4ed40815fa3a305976e753c7a7edb54d6cf2ea1e
Honey-j <l>
2019-10-24 14:13:43 +0800

    修改model/Car.js

---
+++ third-stage/model/Car.js
@@ -0,0 +1,21 @@
+// 创建Type数据库
+
+const mongoose=require('mongoose')
+
+// 设置 Schema 结构
+const CarSchema = new mongoose.Schema({
+    type:String,
+    imgdesc:String,
+    list:Array,
+    smallimage:String,
+    listdesc:String,
+    largelist:Array,
+    largeimage:String,
+    sell:String,
+    img:String
+});
+
+// 设置Model
+const Car = mongoose.model('Car', CarSchema)
+
+module.exports = project
