cb146f17b95f1bdc314d822d6e86df61589ec8d9
sinei-l <2570906949@qq.com>
2019-10-25 17:11:59 +0800

    修改login.js

--- third-stage/api/login.js
+++ third-stage/api/login.js
@@ -1,4 +1,21 @@
-function showLogin(req,res){
-    res.send('ok')
+const User = require('../model/User')
+function login(req,res){
+    User.findOne({username:req.body.username,password:req.body.password})
+    .then((result)=>{
+        if(result){
+            let data = {"username":req.body.username}
+            res.cookie('blog-admin-cookie',JSON.stringify(data));
+            res.json({
+                code:1,
+                msg:'登录成功'
+            })
+        }else{
+            res.json({
+                code:0,
+                msg:'登录失败'
+            })
+        }
+    })
+    // res.send('ok')
 }
-module.exports = showLogin
+module.exports = login
