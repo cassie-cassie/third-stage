e3f531056296dc1d20e36b74be228a6d5b18ce1e
dk1078946033 <1078946033@qq.com>
2019-10-25 16:15:44 +0800

    dk details.js

--- third-stage/controller/details/details.js
+++ third-stage/controller/details/details.js
@@ -1,8 +1,4 @@
 function showdetails(req,res){
     res.render('details')
 }
-<<<<<<< HEAD
 module.exports = showdetails
-=======
-module.exports=showdetails
->>>>>>> 0bb3a4eac2a8c08b515e79e1893a71e43f1e4a5a
