c90ba4a54cd09a26396223c78d31264989053df9
dk1078946033 <1078946033@qq.com>
2019-11-02 23:50:05 +0800

    dk useraddress.js con

--- third-stage/controller/UserAddress.js
+++ third-stage/controller/UserAddress.js
@@ -1,4 +1,27 @@
 function showUseradd(req,res){
-    res.render('UserAddress')
+    res.render('UserAddress',{
+        inform:[{
+            name:"张三",
+            num:"13546210810",
+            address:"山西省晋中市榆次区 米加U聚鑫商务",
+            time:"周六/日收货",
+            delete:"删除",
+            xiugai:"修改"
+        },{
+            name:"李四",
+            num:"13254678954",
+            address:"山西省晋中市榆次区 米加U聚鑫商务",
+            time:"收货时间不限",
+            delete:"删除",
+            xiugai:"修改"
+        },{
+            name:"李四",
+            num:"13254678954",
+            address:"山西省晋中市榆次区 米加U聚鑫商务",
+            time:"收货时间不限",
+            delete:"删除",
+            xiugai:"修改"
+        }]
+    })
 }
 module.exports = showUseradd
